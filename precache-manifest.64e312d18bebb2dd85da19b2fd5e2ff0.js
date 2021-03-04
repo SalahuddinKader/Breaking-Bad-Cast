838d879697c64f5d48e6ed6a31073fdf6a86a98a
salahuddinKader <salahuddinkader007@gmail.com>
2021-03-04 01:53:29 +0000

    Updates

---
+++ precache-manifest.64e312d18bebb2dd85da19b2fd5e2ff0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c29b9a80f9246333a6e9719d4881108f",
+    "url": "/Breaking-Bad-Cast/index.html"
+  },
+  {
+    "revision": "d9811e7399eacbdf2b3c",
+    "url": "/Breaking-Bad-Cast/static/css/main.3155dc01.chunk.css"
+  },
+  {
+    "revision": "c9413a2b0e288bcb1e85",
+    "url": "/Breaking-Bad-Cast/static/js/2.68f4301c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Breaking-Bad-Cast/static/js/2.68f4301c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9811e7399eacbdf2b3c",
+    "url": "/Breaking-Bad-Cast/static/js/main.d44dfd93.chunk.js"
+  },
+  {
+    "revision": "a3b6a786245ad3955008",
+    "url": "/Breaking-Bad-Cast/static/js/runtime-main.8015c9c4.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/Breaking-Bad-Cast/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/Breaking-Bad-Cast/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/Breaking-Bad-Cast/static/media/spinner.235b9b3e.gif"
+  }
+]);
